ab2b5794bfc636835648c72960eee5b5210d14fd
Emmanuel Ukoha <ukohae39@gmail.com>
2023-09-11 02:30:51 -0400

    pull service account

--- variables.tf
+++ variables.tf
@@ -30,3 +30,8 @@ variable "gcp_service_list" {
   description = "List of GCP service to be enabled for a project."
   type        = list(any)
 }
+
+variable "sa" {
+  type = string
+  description = "Service Account"
+}
